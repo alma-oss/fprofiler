67c4491d78e1e4037e545059a783269ff19cca5d
Petr Chromec <petr.chromec@lmc.eu>
2021-01-18 17:39:15 +0100

    Fix git item to be from given values

--- src/Utils.fs
+++ src/Utils.fs
@@ -2,6 +2,19 @@ namespace Lmc.Profiler
 
 [<RequireQualifiedAccess>]
 module List =
+    /// see https://stackoverflow.com/questions/32363848/fastest-way-to-reduce-a-list-based-on-another-list-using-f
+    let filterNotIn excluding list =
+        let toExclude = set excluding
+        list |> List.filter (toExclude.Contains >> not)
+
+    let filterNotInBy f excluding list =
+        let toExclude = set excluding
+        list |> List.filter (f >> toExclude.Contains >> not)
+
+    let filterInBy f including list =
+        let toInclude = set including
+        list |> List.filter (f >> toInclude.Contains)
+
     let takeUpTo limit list =
         if list |> List.length <= limit then list
         else list |> List.take limit
